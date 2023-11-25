cd8fd46690a42f36d0a7c6615c266626c382c554
DeniseRisia <risiaresponde@gmail.com>
2023-11-25 17:41:52 -0300

    add logica ok

--- src/main/java/com/drc/appcarroconsumo/MainActivity.kt
+++ src/main/java/com/drc/appcarroconsumo/MainActivity.kt
@@ -2,10 +2,33 @@ package com.drc.appcarroconsumo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.drc.appcarroconsumo.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding =  ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        binding.buttonResultado.setOnClickListener(this)
+    }
+    override fun onClick(view: View){
+        if (view.id == R.id.buttonResultado){
+            calcularResultado()
+        }
+    }
+    private fun calcularResultado(){
+        val distancia = binding.textDistancia.text.toString().toFloat()
+        val preco = binding.totalPreco.text.toString().toFloat()
+        val autonomia = binding.textAutonomia.text.toString().toFloat()
+
+        val valorTotal = (preco * distancia) / autonomia
+
+        //val valorTotalStr = "R$ ${"%.2f" .format(valorTotal)}"
+
+        binding.textResultado2.text = "R$ ${"%.2f" .format(valorTotal)}"
+        //Toast.makeText(this, valorTotalStr, Toast.LENGTH_SHORT).show()
     }
 }
